d53274bd4ba61605747ea4aed2acf25ddbd4c1a7
pgherveou <pgherveou@gmail.com>
2023-05-25 11:53:54 +0200

    faucet with extension

--- lib.rs
+++ lib.rs
@@ -1,7 +1,48 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
+use ink::env::Environment;
 use ink::primitives::AccountId;
 use sp_runtime::MultiAddress;
+
+#[ink::chain_extension]
+pub trait FetchAsset {
+    type ErrorCode = FetchAssetError;
+
+    #[ink(extension = 1)]
+    fn transfer_token(token_id: u32, amount: u128);
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum FetchAssetError {
+    FetchAssetFailed,
+}
+
+impl ink::env::chain_extension::FromStatusCode for FetchAssetError {
+    fn from_status_code(status_code: u32) -> Result<(), Self> {
+        match status_code {
+            0 => Ok(()),
+            1 => Err(Self::FetchAssetFailed),
+            _ => panic!("encountered unknown status code"),
+        }
+    }
+}
+
+impl Environment for CustomEnvironment {
+    const MAX_EVENT_TOPICS: usize = <ink::env::DefaultEnvironment as Environment>::MAX_EVENT_TOPICS;
+
+    type AccountId = <ink::env::DefaultEnvironment as Environment>::AccountId;
+    type Balance = <ink::env::DefaultEnvironment as Environment>::Balance;
+    type Hash = <ink::env::DefaultEnvironment as Environment>::Hash;
+    type BlockNumber = <ink::env::DefaultEnvironment as Environment>::BlockNumber;
+    type Timestamp = <ink::env::DefaultEnvironment as Environment>::Timestamp;
+
+    type ChainExtension = FetchAsset;
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+pub enum CustomEnvironment {}
 
 #[derive(scale::Encode)]
 enum RuntimeCall {
@@ -22,7 +63,7 @@ enum AssetsCall {
     },
 }
 
-#[ink::contract]
+#[ink::contract(env = crate::CustomEnvironment)]
 mod faucet {
     use crate::{AssetsCall, RuntimeCall};
     use ink::env::debug_println;
@@ -73,6 +114,25 @@ mod faucet {
                     target: self.env().caller().into(),
                     amount: 1,
                 }))
+                .map_err(|_| FaucetError::TransferFailed)
+        }
+
+        #[ink(message)]
+        pub fn drip_chain_extension(&mut self) -> Result<(), FaucetError> {
+            let now = self.env().block_timestamp();
+            let caller = self.env().caller();
+            let last_access_time = self.last_access_time.get(caller).unwrap_or_default();
+            let elapsed_time = now - last_access_time;
+
+            debug_println!("elapsed_time:  {elapsed_time}");
+            if elapsed_time < 10 * 1000 {
+                return Err(FaucetError::DripTooOften);
+            }
+
+            self.last_access_time.insert(self.env().caller(), &now);
+            self.env()
+                .extension()
+                .transfer_token(0, 1)
                 .map_err(|_| FaucetError::TransferFailed)
         }
     }
